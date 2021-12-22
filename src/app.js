1d68b42b658e1017cc89470d521cfb9ac0037d53
SoumadeepBagui409 <75625374+SoumadeepBagui409@users.noreply.github.com>
2021-12-22 11:05:42 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -112,6 +115,6 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("server running");
 })
